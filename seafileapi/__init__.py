d74798425c6d29a737232301f631850c57fa5a2a
Tobias Galitzien <tg@trusttheadmin.de>
2020-04-03 05:23:40 +0200

    colorful debug output

--- seafileapi/__init__.py
+++ seafileapi/__init__.py
@@ -1,5 +1,5 @@
 from seafileapi.client import SeafileApiClient
 
-def connect(server, username, password):
-    client = SeafileApiClient(server, username, password)
+def connect(server, username, password, debug=False):
+    client = SeafileApiClient(server, username, password, None, debug)
     return client
--- seafileapi/client.py
+++ seafileapi/client.py
@@ -3,15 +3,27 @@ from seafileapi.utils import urljoin
 from seafileapi.exceptions import ClientHttpError
 from seafileapi.repos import Repos
 
+class c:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARN = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 class SeafileApiClient(object):
     """Wraps seafile web api"""
-    def __init__(self, server, username=None, password=None, token=None):
+    def __init__(self, server, username=None, password=None, token=None, debug=False):
         """Wraps various basic operations to interact with seahub http api.
         """
         self.server = server
         self.username = username
         self.password = password
         self._token = token
+        self.debug = debug
 
         self.repos = Repos(self)
         self.groups = Groups(self)
@@ -25,7 +37,12 @@ class SeafileApiClient(object):
             'password': self.password,
         }
         url = urljoin(self.server, '/api2/auth-token/')
+        if self.debug:
+            print(f'{c.FAIL}REQUEST: %s %s{c.END}' % (url, data))
         res = requests.post(url, data=data)
+        if self.debug:
+            print(f'{c.OKBLUE}REPLY:   %s{c.END}' % res.content)
+            print('-' * 120)
         if res.status_code != 200:
             raise ClientHttpError(res.status_code, res.content)
         token = res.json()['token']
@@ -53,14 +70,22 @@ class SeafileApiClient(object):
         if not url.startswith('http'):
             url = urljoin(self.server, url)
 
-        headers = kwargs.get('headers', {})
-        headers.setdefault('Authorization', 'Token ' + self._token)
-        kwargs['headers'] = headers
+        if not '/seafhttp/' in url:
+            headers = kwargs.get('headers', {})
+            headers.setdefault('Authorization', 'Token ' + self._token)
+            kwargs['headers'] = headers
 
         expected = kwargs.pop('expected', 200)
         if not hasattr(expected, '__iter__'):
             expected = (expected, )
+        if self.debug:
+            print(f'{c.HEADER}REQUEST: %s %s %s %s{c.END}' % (method, url, args, kwargs))
         resp = requests.request(method, url, *args, **kwargs)
+        if self.debug:
+            if resp.request.method == 'POST':
+                print(f'{c.WARN+c.BOLD}POSTBODY: %s{c.END}' % resp.request.body)
+            print(f'{c.OKGREEN}REPLY:   %s{c.END}' % resp.content)
+            print('-' * 120)
         if resp.status_code not in expected:
             msg = 'Expected %s, but get %s' % \
                   (' or '.join(map(str, expected)), resp.status_code)
